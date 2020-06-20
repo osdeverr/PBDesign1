3711b5222f6aea447775c40c43d603bad4ada058
osdever <krasota156@gmail.com>
2020-06-20 14:27:53 +0300

    Documented most of the code

--- PBDesign1/pcl/policies/threadmodel.h
+++ PBDesign1/pcl/policies/threadmodel.h
@@ -3,9 +3,26 @@
 
 namespace pcl::policies
 {
+	/// <summary>
+	/// Represents a "tag" to distinguish threading models from other policies.
+	/// You must inherit from this to mark your policy as a thread policy.
+	/// </summary>
 	struct thread_model
 	{};
 
+	/// <summary>
+	/// A dummy thread policy doing nothing.
+	/// </summary>
+	class thread_unsafe : thread_model
+	{
+	public:
+		void lock() {}
+		void unlock() {}
+	};
+
+	/// <summary>
+	/// A simple mutex-based thread policy. Can be used with lock_guard since it's a BasicLockable.
+	/// </summary>
 	class thread_safe : thread_model
 	{
 	public:
@@ -13,12 +30,5 @@ namespace pcl::policies
 		void unlock() { mLock.unlock(); }
 	private:
 		std::mutex mLock;
-	};
-
-	class thread_unsafe : thread_model
-	{
-	public:
-		void lock() {}
-		void unlock() {}
 	};
 }
--- PBDesign1/pcl/vectors/dzen.h
+++ PBDesign1/pcl/vectors/dzen.h
@@ -3,14 +3,46 @@
 
 namespace pcl::vectors
 {
+	/// <summary>
+	/// Represents a Dzen vector. Exists for the sole reason of shits and giggles
+	/// </summary>
+	/// <typeparam name="T">The value type of this Dzen vector.</typeparam>
+	/// <typeparam name="ThreadModel">The thread model to use in this Dzen vector: <see cref="pcl::policies::thread_unsafe"/> by default.</typeparam>
 	template<class T, class ThreadModel = pcl::policies::thread_unsafe>
 	class dzen
 	{
 	public:
-		T& get() { std::lock_guard lg(mThreadModel); return mValue; }
-		const T& get() const { std::lock_guard lg(mThreadModel); return mValue; }
+		static_assert(std::is_base_of_v<pcl::policies::thread_model, ThreadModel>, "A ThreadModel specified does not have the 'thread_policy' tag");
 
-		void set(const T& val) { std::lock_guard lg(mThreadModel); mValue = val; }
+		/// <summary>
+		/// Gets a reference to the internal value.
+		/// </summary>
+		/// <returns>A reference to this Dzen vector's internal value.</returns>
+		T& get()
+		{
+			std::lock_guard lg(mThreadModel);
+			return mValue;
+		}
+
+		/// <summary>
+		/// Gets a const reference to the internal value: overloads get() for const instances of the Dzen vector.
+		/// </summary>
+		/// <returns>A const reference to this Dzen vector's internal value.</returns>
+		const T& get() const
+		{
+			std::lock_guard lg(mThreadModel);
+			return mValue;
+		}
+
+		/// <summary>
+		/// Sets the internal value of this Dzen vector.
+		/// </summary>
+		/// <param name="val">The new value of this Dzen vector.</param>
+		void set(const T& val)
+		{
+			std::lock_guard lg(mThreadModel);
+			mValue = val;
+		}
 	private:
 		ThreadModel mThreadModel{};
 		T mValue{};
