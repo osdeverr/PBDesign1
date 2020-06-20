a70bf5cf4cc269f76054bf90224697e7f1aaf612
osdever <krasota156@gmail.com>
2020-06-20 19:21:30 +0300

    Added some logging just for lulz

--- PBDesign1/PBDesign1.cpp
+++ PBDesign1/PBDesign1.cpp
@@ -4,12 +4,76 @@
 #include <pcl/policies/invalid.h>
 #include <pcl/vectors/dzen.h>
 #include <pcl/hashing/autohash.h>
+
 #include <fmt/format.h>
+#include <fmt/color.h>
+
+enum class level
+{
+	debug,
+	fixme,
+	info,
+	warn,
+	error,
+};
+
+template<level Level>
+void print_warn_header();
+
+template<>
+void print_warn_header<level::debug>()
+{
+	using namespace fmt;
+	print(fg(color::dark_slate_gray), "debug");
+}
+
+template<>
+void print_warn_header<level::fixme>()
+{
+	using namespace fmt;
+	print(fg(color::pale_violet_red), "fixme");
+}
+
+template<>
+void print_warn_header<level::info>()
+{
+	using namespace fmt;
+	print(fg(color::green), "info");
+}
+
+template<>
+void print_warn_header<level::warn>()
+{
+	using namespace fmt;
+	print(fg(color::yellow), "warn");
+}
+
+template<>
+void print_warn_header<level::error>()
+{
+	using namespace fmt;
+	print(fg(color::indian_red), "error");
+}
+
+template<level Level, typename... Args>
+void logw(const std::string& format, Args... args)
+{
+	using namespace fmt;
+	print(fg(color::dim_gray), "[");
+	print_warn_header<Level>();
+	print(fg(color::dim_gray), "] ");
+	print(format, args...);
+	print("\n");
+}
 
 using myhash = pcl::hashing::autohash<std::string>;
 void DoPlaks(myhash h)
 {
-	fmt::print("Resolved: '{}'", h.resolve());
+	logw<level::debug>("Resolved: '{}'", h.resolve());
+	logw<level::fixme>("Resolved: '{}'", h.resolve());
+	logw<level::info>("Resolved: '{}'", h.resolve());
+	logw<level::warn>("Resolved: '{}'", h.resolve());
+	logw<level::error>("Resolved: '{}'", h.resolve());
 }
 
 int main()
